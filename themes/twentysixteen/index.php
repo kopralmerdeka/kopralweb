b487f1ff3940eb02c6d9f3e8b0363a2d14d0ce44
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-01-16 01:58:34 +0700

    Social media button, carousel, minor fixes

--- themes/twentysixteen/index.php
+++ themes/twentysixteen/index.php
@@ -58,7 +58,7 @@ get_header(); ?>
 					<button class="custom-button custom-button-blue">Yang bergabung...</button>
 				</div>
 				<div class="member-content entry-content">
-					<div>
+					<div id="member">
 						<!--Iteration example-->
 				    <!--div class="member-item">
 				    	<div class="member-item-logo"></div>
@@ -68,38 +68,79 @@ get_header(); ?>
 				    	<div class="member-item-logo"></div>
 				    	<div class="member-item-title">BelajarFreeBSD</div>
 				    </div-->
-<?php
-//sort users descending by number of posts, display username and avatar
-$uc=array();
-$blogusers = get_users_of_blog();
-if ($blogusers) {
-  foreach ($blogusers as $bloguser) {
-    $post_count = get_usernumposts($bloguser->user_id);
-    $uc[$bloguser->user_id]=$post_count;
-  }
-  arsort($uc);
-  foreach ($uc as $key => $value) {
-    $user = get_userdata($key);
-    $author_posts_url = get_author_posts_url($key);
-    $post_count = $value;
-				    echo "<div class=\"member-item\" onclick=\"location.href='".$author_posts_url."';\">";
-				    echo "<div class=\"member-item-logo\">";
- 						echo get_avatar( $user->ID, 170 );
-						echo "</div>";
-				    echo "<div class=\"member-item-title\">".$user->user_nicename."</div>";
-				    echo "</div>";
-  }
-}
-?>
+						<div class="jcarousel-wrapper">
+                <div class="jcarousel">
+                    <ul>
+                      <?php
+                      //sort users descending by number of posts, display username and avatar
+                      $uc=array();
+                      $blogusers = get_users_of_blog();
+                      if ($blogusers) {
+                        foreach ($blogusers as $bloguser) {
+                          $post_count = get_usernumposts($bloguser->user_id);
+                          $uc[$bloguser->user_id]=$post_count;
+                        }
+                        arsort($uc);
+                        foreach ($uc as $key => $value) {
+                          $user = get_userdata($key);
+                          $author_posts_url = get_author_posts_url($key);
+                          $post_count = $value;
+                            if ($user->user_nicename != "admin") {
+                      						echo "<li>";
+                      				    echo "<div class=\"member-item\" onclick=\"location.href='".$author_posts_url."';\">";
+                      				    echo "<div class=\"member-item-logo\">";
+                       						echo get_avatar( $user->ID, 170 );
+                      						echo "</div>";
+                      				    echo "<div class=\"member-item-title\">".$user->nickname."</div>";
+                      				    echo "</div>";
+                      						echo "</li>";
+                            }
+                        }
+                      }
+                      ?>
+                    </ul>
+                </div>
+
+
+                <p class="jcarousel-pagination"></p>
+            </div>
 					</div>
 				</div>
+        <script type="text/javascript" src="wp-content/themes/twentysixteen/lib/carousel.js"></script>
+        <script type="text/javascript">
+          jQuery(function($) {
+          	var jcarousel = $('.jcarousel');
+            jcarousel
+              .on('jcarousel:reload jcarousel:create', function () {
+                  var carousel = $(this),
+                      width = carousel.innerWidth();
+                  if (width >= 600) {
+                      width = width / 3;
+                  } else if (width >= 350) {
+                      width = width / 2;
+                  }
+                  carousel.jcarousel('items').css('width', Math.ceil(width) + 'px');
+              })
+              .jcarousel({
+                  wrap: 'circular'
+              });
+              $('.jcarousel').jcarouselAutoscroll({
+                  interval: 3000
+              });
+          });
+        </script>
       </div><!--End of block-->
 			<div id="register" class="register-container">
 				<div class="register-title">Mari bergabung</div>
 				<div class="entry-content">
-<?php
-echo do_shortcode("[wppb-register]");
-?>
+        <?php
+        	echo do_shortcode("[wppb-register]");
+        ?>
+        <script>
+        var passwd = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 30);
+        document.getElementById("passw1").value = passwd;
+        document.getElementById("passw2").value = passwd;
+        </script>
 				</div>
       </div><!--End of block-->
     </div>
